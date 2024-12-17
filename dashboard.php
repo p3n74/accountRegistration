b2230baf450241a36679ba548ce38661d7872423
pentuhhh <firefall.challenger@gmail.com>
2024-12-18 04:14:32 +0800

    icons

--- dashboard.php
+++ dashboard.php
@@ -99,6 +99,8 @@ $conn->close();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
+ <!-- Favicon -->
+ <link rel="icon" href="icon.png" type="image/png">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
