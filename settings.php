0a5999347b9b40104d7ae1e145291d029eb49c12
pentuhhh <firefall.challenger@gmail.com>
2024-12-18 03:34:50 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -214,18 +214,25 @@ $conn->close();
 </head>
 <body>
   <div class="d-flex">
-    <!-- Sidebar -->
+    <!-- Sidebar with profile information -->
     <div class="sidebar col-md-3 col-lg-2 p-3">
       <div class="text-center">
+        <!-- Display profile picture -->
         <img src="<?php echo htmlspecialchars($profilepicture); ?>" alt="User Profile" class="img-fluid">
         <h4><?php echo htmlspecialchars($fname . ' ' . $lname); ?></h4>
         <p><?php echo htmlspecialchars($email); ?></p>
       </div>
       <hr>
       <ul class="nav flex-column">
-        <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
-        <li class="nav-item"><a class="nav-link active" href="settings.php">Settings</a></li>
-        <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+        <li class="nav-item">
+          <a class="nav-link" href="dashboard.php">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="settings.php">Settings</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">Logout</a>
+        </li>
       </ul>
     </div>
 
