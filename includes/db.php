90d77eb0d2752f279602be1f2c19214197e8ce92
p3n74 <firefall.challenger@gmail.com>
2024-12-21 20:39:12 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "s21102134_palisade"; 
-$password = "webwebwebweb";
-$dbname = "s21102134_palisade";
+$username = ""; 
+$password = "";
+$dbname = "";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
